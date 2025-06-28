47c97d2bc2fde36e9427e9b1742d4101d75209af
Mihajlo <milanovic.mihajlo.8@gmail.com>
2025-06-28 15:05:58 +0200

    Fixed scss in demo editor

--- rtce/src/app/page.tsx
+++ rtce/src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-
-import Tiptap from '../../components/Tiptap'
 import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
 
 export default function Home() {
