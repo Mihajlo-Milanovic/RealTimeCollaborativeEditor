a7f9f009c70bb33dafc5b44c93c3cfb607a788c9
Mihajlo <milanovic.mihajlo.8@gmail.com>
2025-07-24 22:35:10 +0200

    Minor refactoring

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,5 +1,10 @@
 import {configDotenv} from "dotenv";
 
 configDotenv();
-export const port= process.env.PORT || 5000
-export const mongoURI= process.env.MONGO_URI || ""
+export const port = process.env.PORT || 5000
+export const mongoURI = process.env.MONGO_URI || ""
+
+export const nextAuthUrl = process.env.NEXTAUTH_URL || ""
+
+export const emailUser = process.env.EMAIL_USER || ""
+export const emailAppPass = process.env.EMAIL_APP_PASS || ""
--- backend/src/routes/commentRoutes.ts
+++ backend/src/routes/commentRoutes.ts
@@ -7,5 +7,5 @@ export const commentRouter = express.Router();
 commentRouter.get('/getCommentById', cc.getCommentById);
 commentRouter.get('/getCommentsForFile', cc.getCommentsForFile);
 commentRouter.post('/createComment', cc.createComment);
-commentRouter.delete('/deleteComment', cc.deleteComment);
 commentRouter.put('/updateComment', cc.updateComment);
+commentRouter.delete('/deleteComment', cc.deleteComment);
